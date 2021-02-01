8114daa3998df24cbb3582df79cc6f329b4f89c9
Leonardo <lordghick@gmail.com>
2021-01-31 21:41:18 -0400

    Reubicados

---
+++ scripts/scripts.js
@@ -0,0 +1,17 @@
+
+// Categorias
+
+let categorias = ['General', 'Personal', 'Trabajo'];
+
+function listarCategorias() {
+    let select = document.getElementById('listSelect');
+    for (let i = 0; i < categorias.length; i++) {
+        let option = document.createElement("option");
+        option.text = categorias[i];
+        option.value = i;
+        select.add(option)
+    }
+}
+
+listarCategorias();
+
