c67dbedcbadd693e2b018cca1f67a7cd02ec6279
Leonardo <lordghick@gmail.com>
2021-02-11 15:02:25 -0400

    estilizando modal

--- plantillas/modalTarea.php
+++ plantillas/modalTarea.php
@@ -1,29 +1,40 @@
 <div id="modalContainer" class="modalContainer">
     <div id="modalNuevaTarea" class="modalNuevaTarea">
-        <button type="button" id="cerrarModal" onclick="cerrarModal()">X</button>
-        <form method="post" action="app/tareaNueva.php">
-            <div>
-                <select id="listSelect" name="categoria"></select>
+        <div class="btn-modal-box">
+            <button type="button" class="btn-modal" id="cerrarModal" onclick="cerrarModal()"><i class="exit-btn fas fa-times-circle"></i></button>
+        </div>
+        <form class="form-container" method="post" action="app/tareaNueva.php">
+            <div class="container-select">
+                <div class="container-prioridad">
+                    <select name="prioridad" id="prioridad">
+                        <option value="1">Importante</option>
+                        <option value="2" selected>Normal</option>
+                        <option value="3">Poco relevante</option>
+                    </select>
+                </div>
+                <div class="categoria-container">
+                    <select id="listSelect" name="categoria"></select>
+                </div>
             </div>
-            <div>
-                <input type="text" id="asunto" placeholder="Asunto" name="asunto" required>
+
+            <div class="asunto-container">
+                <input type="text" id="asunto" class="asunto-input" placeholder="Asunto" name="asunto" required>
             </div>
-            <div>
+            <div class="container-detalles">
                 <textarea cols="30" rows="10" placeholder="Detalles" name="detalles"></textarea>
             </div>
-            <div>
-                <p> Hora asignada:
+            <div class="container-select-2">
+                <div class="container-hora">
+                    <label for="hora">Hora asignada:</label>
                     <input type="time" name="hora">
-                </p>
+                    </p>
+                </div>
+                <div class="container-fecha">
+                    <label for="fecha">Fecha asignada:</label>
+                    <input type="date" name="fecha">
+                </div>
             </div>
-            <div>
-                <select name="prioridad" id="prioridad">
-                    <option value="1">Importante</option>
-                    <option value="2" selected>Normal</option>
-                    <option value="3">Poco relevante</option>
-                </select>
-            </div>
-            <button type="submit" name="enviar">Guardar tarea</button>
+            <button type="submit" name="enviar" class="enviar">Guardar tarea</button>
         </form>
     </div>
 </div>
